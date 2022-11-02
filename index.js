c601391a15e311a2c4b08db38cfdd715e8f2453b
Valadaress <57867475+Valadaress@users.noreply.github.com>
2022-11-02 13:22:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { readFile } = require('fs')
+const { request } = require('http')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    readFile('./home.html', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('<html><body><h1>Sorry, out of order</h1></body><html>')
+        }
+        // console.log(data)
+        res.send(data)
+    })
+
+
+})
+
+
+app.listen(process.env.PORT || 3000, console.log(`App is available on http://localhost:3000`))
